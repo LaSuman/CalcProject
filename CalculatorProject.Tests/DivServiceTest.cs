51d1ada4a927ac044c4faa9e0e114c17dad7cd74
Suman Adhikari <adhikari.suman012@gmail.com>
2025-04-15 08:57:48 +1000

    Added test cases for Divison and Multiplication

--- CalculatorProject.Tests/DivServiceTest.cs
+++ CalculatorProject.Tests/DivServiceTest.cs
@@ -33,5 +33,280 @@ namespace CalculatorProject.Tests
             // Assert
             Assert.Equal(2.0, result);
         }
+        [Fact(DisplayName = "Should div two valid number")]
+        public async Task ShouldDivTwoValidNumber()
+        {
+            // Arrange
+            CalculatorRequest request = new CalculatorRequest
+            {
+                Maths = new Maths
+                {
+                    Operation = new Operation
+                    {
+                        ID = nameof(Operator.Division),
+                        Value = new List<string> { "6", "2" }
+                    }
+                }
+
+            };
+            // Act
+            double result = operation.Calculate(request);
+            // Assert
+            Assert.Equal(3.00, result);
+        }
+
+        [Fact(DisplayName = "Should Error for One input ")]
+        public async Task ShouldErrorForOneInput()
+        {
+            // Arrange
+            CalculatorRequest request = new CalculatorRequest
+            {
+                Maths = new Maths
+                {
+                    Operation = new Operation
+                    {
+                        ID = nameof(Operator.Division),
+                        Value = new List<string> { "3" }
+                    }
+                }
+
+            };
+            // Act
+            double result = operation.Calculate(request);
+            // Assert
+            Assert.Equal(3.00, result);
+        }
+
+        [Fact(DisplayName = "Should div with 0 as infinity")]
+        public async Task ShoulddivWith0AsInfinity()
+        {
+            // Arrange
+            CalculatorRequest request = new CalculatorRequest
+            {
+                Maths = new Maths
+                {
+                    Operation = new Operation
+                    {
+                        ID = nameof(Operator.Division),
+                        Value = new List<string> { "6", "0" }
+                    }
+                }
+
+            };
+            // Act
+            double result = operation.Calculate(request);
+            // Assert
+            Assert.Equal(1, result);
+        }
+
+        [Fact(DisplayName = "Should div negative number ")]
+        public async Task ShouldDivNegativeNumber()
+        {
+            // Arrange
+            CalculatorRequest request = new CalculatorRequest
+            {
+                Maths = new Maths
+                {
+                    Operation = new Operation
+                    {
+                        ID = nameof(Operator.Division),
+                        Value = new List<string> { "-30", "-3" }
+                    }
+                }
+
+            };
+            // Act
+            double result = operation.Calculate(request);
+            // Assert
+            Assert.Equal(10.00, result);
+        }
+
+        [Fact(DisplayName = "Should handle large number ")]
+        public async Task ShouldHandleLargeNumber()
+        {
+            // Arrange
+            CalculatorRequest request = new CalculatorRequest
+            {
+                Maths = new Maths
+                {
+                    Operation = new Operation
+                    {
+                        ID = nameof(Operator.Division),
+                        Value = new List<string> { "3000000", "1000000" }
+                    }
+                }
+
+            };
+            // Act
+            double result = operation.Calculate(request);
+            // Assert
+            Assert.Equal(3, result);
+        }
+
+        [Fact(DisplayName = "Should handle decimal number ")]
+        public async Task ShouldHandleDecimalNumber()
+        {
+            // Arrange
+            CalculatorRequest request = new CalculatorRequest
+            {
+                Maths = new Maths
+                {
+                    Operation = new Operation
+                    {
+                        ID = nameof(Operator.Division),
+                        Value = new List<string> { "15.0", "1.5" }
+                    }
+                }
+            };
+            // Act
+            double result = operation.Calculate(request);
+            // Assert
+            Assert.Equal(1.00, result);
+        }
+
+
+        [Fact(DisplayName = "Should handle empty input ")]
+        public async Task ShouldHandleEmptyInput()
+        {
+            // Arrange
+            CalculatorRequest request = new CalculatorRequest
+            {
+                Maths = new Maths
+                {
+                    Operation = new Operation
+                    {
+                        ID = nameof(Operator.Division),
+                        Value = new List<string> { }
+                    }
+                }
+            };
+            // Act
+            double result = operation.Calculate(request);
+            // Assert
+            Assert.Equal(0.00, result);
+        }
+
+        [Fact(DisplayName = "Should throw exception different format input ")]
+        public async Task ShouldThrowExceptionDifferentFormatInput()
+        {
+            // Arrange
+            CalculatorRequest request = new CalculatorRequest
+            {
+                Maths = new Maths
+                {
+                    Operation = new Operation
+                    {
+                        ID = nameof(Operator.Division),
+                        Value = new List<string>() { "1", "abc" }
+                    }
+                }
+            };
+            // Act
+            double result = operation.Calculate(request);
+            // Assert
+            Assert.Throws<FormatException>(() => result);
+        }
+
+        [Fact(DisplayName = "Should throw exception invalid operation ")]
+        public async Task ShouldThrowExceptionInvalidOperation()
+        {
+            // Arrange
+            CalculatorRequest request = new CalculatorRequest
+            {
+                Maths = new Maths
+                {
+                    Operation = new Operation
+                    {
+                        ID = "InvalidOperation",
+                        Value = new List<string>() { "1", "2" }
+                    }
+                }
+            };
+            // Act
+            double result = operation.Calculate(request);
+            // Assert
+            Assert.Throws<InvalidOperationException>(() => result);
+        }
+
+        [Fact(DisplayName = "Should throw exception null input ")]
+        public async Task ShouldThrowExceptionNullInput()
+        {
+            // Arrange
+            CalculatorRequest request = new CalculatorRequest
+            {
+                Maths = new Maths
+                {
+                    Operation = null
+                }
+            };
+            // Act
+            double result = operation.Calculate(request);
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => result);
+        }
+
+        [Fact(DisplayName = "Should calculate leading zeros ")]
+        public async Task ShouldCalculateLeadingZeros()
+        {
+            // Arrange
+            CalculatorRequest request = new CalculatorRequest
+            {
+                Maths = new Maths
+                {
+                    Operation = new Operation
+                    {
+                        ID = nameof(Operator.Division),
+                        Value = new List<string> { "0003", "0002" }
+                    }
+                }
+            };
+            // Act
+            double result = operation.Calculate(request);
+            // Assert
+            Assert.Equal(1.5, result);
+        }
+        [Fact(DisplayName = "Should calculate trailing zeros ")]
+        public async Task ShouldCalculateTrailingZeros()
+        {
+            // Arrange
+            CalculatorRequest request = new CalculatorRequest
+            {
+                Maths = new Maths
+                {
+                    Operation = new Operation
+                    {
+                        ID = nameof(Operator.Division),
+                        Value = new List<string> { "3.00", "2.00" }
+                    }
+                }
+            };
+            // Act
+            double result = operation.Calculate(request);
+            // Assert
+            Assert.Equal(1.5, result);
+        }
+
+        [Fact(DisplayName = "Should skip empty values and calculate valid ones")]
+        public async Task ShouldSkipEmptyValues()
+        {
+            // Arrange
+            var request = new CalculatorRequest
+            {
+                Maths = new Maths
+                {
+                    Operation = new Operation
+                    {
+                        ID = nameof(Operator.Division),
+                        Value = new List<string> { "5", "", "10" }
+                    }
+                }
+            };
+
+            // Act
+            double result = operation.Calculate(request);
+
+            // Assert
+            Assert.Equal(15.00, result); // Empty string is treated as 0 or skipped
+        }
     }
 }
--- CalculatorProject.Tests/MulServiceTest.cs
+++ CalculatorProject.Tests/MulServiceTest.cs
@@ -35,5 +35,280 @@ namespace CalculatorProject.Tests
             Assert.Equal(10.0, result);
 
         }
+        [Fact(DisplayName = "Should mul two valid number")]
+        public async Task ShouldMulTwoValidNumber()
+        {
+            // Arrange
+            CalculatorRequest request = new CalculatorRequest
+            {
+                Maths = new Maths
+                {
+                    Operation = new Operation
+                    {
+                        ID = nameof(Operator.Multiplication),
+                        Value = new List<string> { "3", "2" }
+                    }
+                }
+
+            };
+            // Act
+            double result = operation.Calculate(request);
+            // Assert
+            Assert.Equal(6.00, result);
+        }
+
+        [Fact(DisplayName = "Should Mul One valid number ")]
+        public async Task ShouldMulOneValidNumber()
+        {
+            // Arrange
+            CalculatorRequest request = new CalculatorRequest
+            {
+                Maths = new Maths
+                {
+                    Operation = new Operation
+                    {
+                        ID = nameof(Operator.Multiplication),
+                        Value = new List<string> { "3" }
+                    }
+                }
+
+            };
+            // Act
+            double result = operation.Calculate(request);
+            // Assert
+            Assert.Equal(0.00, result);
+        }
+
+        [Fact(DisplayName = "Should Mul with 0")]
+        public async Task ShouldMulWith0()
+        {
+            // Arrange
+            CalculatorRequest request = new CalculatorRequest
+            {
+                Maths = new Maths
+                {
+                    Operation = new Operation
+                    {
+                        ID = nameof(Operator.Multiplication),
+                        Value = new List<string> { "6", "0" }
+                    }
+                }
+
+            };
+            // Act
+            double result = operation.Calculate(request);
+            // Assert
+            Assert.Equal(0.00, result);
+        }
+
+        [Fact(DisplayName = "Should mul negative number ")]
+        public async Task ShouldMulNegativeNumber()
+        {
+            // Arrange
+            CalculatorRequest request = new CalculatorRequest
+            {
+                Maths = new Maths
+                {
+                    Operation = new Operation
+                    {
+                        ID = nameof(Operator.Multiplication),
+                        Value = new List<string> { "-3", "-10" }
+                    }
+                }
+
+            };
+            // Act
+            double result = operation.Calculate(request);
+            // Assert
+            Assert.Equal(30.00, result);
+        }
+
+        [Fact(DisplayName = "Should handle large number ")]
+        public async Task ShouldHandleLargeNumber()
+        {
+            // Arrange
+            CalculatorRequest request = new CalculatorRequest
+            {
+                Maths = new Maths
+                {
+                    Operation = new Operation
+                    {
+                        ID = nameof(Operator.Multiplication),
+                        Value = new List<string> { "3000000", "1000000" }
+                    }
+                }
+
+            };
+            // Act
+            double result = operation.Calculate(request);
+            // Assert
+            Assert.Equal(3000000000000.00, result);
+        }
+
+        [Fact(DisplayName = "Should handle decimal number ")]
+        public async Task ShouldHandleDecimalNumber()
+        {
+            // Arrange
+            CalculatorRequest request = new CalculatorRequest
+            {
+                Maths = new Maths
+                {
+                    Operation = new Operation
+                    {
+                        ID = nameof(Operator.Multiplication),
+                        Value = new List<string> { "2.5", "2.5" }
+                    }
+                }
+            };
+            // Act
+            double result = operation.Calculate(request);
+            // Assert
+            Assert.Equal(6.25, result);
+        }
+
+
+        [Fact(DisplayName = "Should handle empty input ")]
+        public async Task ShouldHandleEmptyInput()
+        {
+            // Arrange
+            CalculatorRequest request = new CalculatorRequest
+            {
+                Maths = new Maths
+                {
+                    Operation = new Operation
+                    {
+                        ID = nameof(Operator.Multiplication),
+                        Value = new List<string> { }
+                    }
+                }
+            };
+            // Act
+            double result = operation.Calculate(request);
+            // Assert
+            Assert.Equal(0.00, result);
+        }
+
+        [Fact(DisplayName = "Should throw exception different format input ")]
+        public async Task ShouldThrowExceptionDifferentFormatInput()
+        {
+            // Arrange
+            CalculatorRequest request = new CalculatorRequest
+            {
+                Maths = new Maths
+                {
+                    Operation = new Operation
+                    {
+                        ID = nameof(Operator.Multiplication),
+                        Value = new List<string>() { "1", "abc" }
+                    }
+                }
+            };
+            // Act
+            double result = operation.Calculate(request);
+            // Assert
+            Assert.Throws<FormatException>(() => result);
+        }
+
+        [Fact(DisplayName = "Should throw exception invalid operation ")]
+        public async Task ShouldThrowExceptionInvalidOperation()
+        {
+            // Arrange
+            CalculatorRequest request = new CalculatorRequest
+            {
+                Maths = new Maths
+                {
+                    Operation = new Operation
+                    {
+                        ID = "InvalidOperation",
+                        Value = new List<string>() { "1", "2" }
+                    }
+                }
+            };
+            // Act
+            double result = operation.Calculate(request);
+            // Assert
+            Assert.Throws<InvalidOperationException>(() => result);
+        }
+
+        [Fact(DisplayName = "Should throw exception null input ")]
+        public async Task ShouldThrowExceptionNullInput()
+        {
+            // Arrange
+            CalculatorRequest request = new CalculatorRequest
+            {
+                Maths = new Maths
+                {
+                    Operation = null
+                }
+            };
+            // Act
+            double result = operation.Calculate(request);
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => result);
+        }
+
+        [Fact(DisplayName = "Should calculate leading zeros ")]
+        public async Task ShouldCalculateLeadingZeros()
+        {
+            // Arrange
+            CalculatorRequest request = new CalculatorRequest
+            {
+                Maths = new Maths
+                {
+                    Operation = new Operation
+                    {
+                        ID = nameof(Operator.Multiplication),
+                        Value = new List<string> { "0003", "0002" }
+                    }
+                }
+            };
+            // Act
+            double result = operation.Calculate(request);
+            // Assert
+            Assert.Equal(6.00, result);
+        }
+        [Fact(DisplayName = "Should calculate trailing zeros ")]
+        public async Task ShouldCalculateTrailingZeros()
+        {
+            // Arrange
+            CalculatorRequest request = new CalculatorRequest
+            {
+                Maths = new Maths
+                {
+                    Operation = new Operation
+                    {
+                        ID = nameof(Operator.Multiplication),
+                        Value = new List<string> { "3.00", "2.00" }
+                    }
+                }
+            };
+            // Act
+            double result = operation.Calculate(request);
+            // Assert
+            Assert.Equal(6.00, result);
+        }
+
+        [Fact(DisplayName = "Should skip empty values and calculate valid ones")]
+        public async Task ShouldSkipEmptyValues()
+        {
+            // Arrange
+            var request = new CalculatorRequest
+            {
+                Maths = new Maths
+                {
+                    Operation = new Operation
+                    {
+                        ID = nameof(Operator.Multiplication),
+                        Value = new List<string> { "5", "", "10" }
+                    }
+                }
+            };
+
+            // Act
+            double result = operation.Calculate(request);
+
+            // Assert
+            Assert.Equal(15.00, result); // Empty string is treated as 0 or skipped
+        }
     }
 }
