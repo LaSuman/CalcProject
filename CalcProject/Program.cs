6224c93618fd548a31e841791c57bae5110598ed
Suman Adhikari <adhikari.suman012@gmail.com>
2025-04-15 11:25:16 +1000

    Added xml formatter

--- CalcProject/Program.cs
+++ CalcProject/Program.cs
@@ -7,6 +7,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Add XML serializer formatters
+builder.Services.AddControllers().AddXmlSerializerFormatters();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
